9d954a7366b3401e6545f76f7cfc42ae620a2403
fileaccent <x863557386@163.com>
2020-02-17 03:11:45 +0800

    设置像素点解决截图模糊问题

--- js/share.js
+++ js/share.js
@@ -164,9 +164,10 @@ function convert2canvas() {
   var width = shareContent.offsetWidth; //获取dom 宽度
   var height = shareContent.offsetHeight; //获取dom 高度
   var canvas = document.createElement("canvas"); //创建一个canvas节点
-  var scale = 2; //定义任意放大倍数 支持小数
+  var scale = 1; //定义任意放大倍数 支持小数
   canvas.width = width * scale; //定义canvas 宽度 * 缩放
   canvas.height = height * scale; //定义canvas高度 *缩放
+  console.log(canvas.width, canvas.height);
   //放大后再缩小提高清晰度
   canvas.getContext("2d").scale(scale, scale);
   //document.body.append(canvas);
@@ -180,7 +181,9 @@ function convert2canvas() {
     // logging: true, //日志开关，便于查看html2canvas的内部执行流程
     width: width, //dom 原始宽度
     height: height,
-    useCORS: true // 【重要】开启跨域配置
+    dpi: window.devicePixelRatio,
+    useCORS: true, // 【重要】开启跨域配置
+    scale:2
   };
   // 开始转化为canvs对象
   html2canvas(shareContent, opts).then(function(canvas) {
