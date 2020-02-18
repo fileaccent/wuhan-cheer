0d70ffb17f61fcb7e5257c8bdb19d11eebba67bb
fileaccent <x863557386@163.com>
2020-02-18 23:44:58 +0800

    最后的调试

--- js/share.js
+++ js/share.js
@@ -1,2 +1,216 @@
-!function(e){var t={};function o(n){if(t[n])return t[n].exports;var r=t[n]={i:n,l:!1,exports:{}};return e[n].call(r.exports,r,r.exports,o),r.l=!0,r.exports}o.m=e,o.c=t,o.d=function(e,t,n){o.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},o.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.t=function(e,t){if(1&t&&(e=o(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(o.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)o.d(n,r,function(t){return e[t]}.bind(null,r));return n},o.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(t,"a",t),t},o.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},o.p="",o(o.s=0)}([function(e,t,o){"use strict";var n,r,a=echarts.init(document.getElementById("china-map")),i=JSON.parse(window.sessionStorage.getItem("grobal")),c=["#1f6d8e","#33a4af","#70d4de","#bce6ea","#e5eabc","#eeeeee"];!function(){console.log(i);var e=new Image;e.setAttribute("crossOrigin","anonymous");var t=document.getElementById("canvas"),o=t.getContext("2d");e.src=i.avatar,e.onload=function(){t.width=e.width,t.height=e.height,o.drawImage(e,0,0)},document.querySelector(".message").innerHTML="<div>我在"+i.province+"</div><div>我为湖北加油! 我为中国加油!</div>",console.log(window.location);new QRCode(document.querySelector(".QRcode"),{text:window.location.href.split("?")[0].replace("share.html","index.html"),colorDark:"#000000",colorLight:"#ffffff",correctLevel:QRCode.CorrectLevel.H});var n=document.querySelector(".QRcode > canvas"),r=Canvas2Image.convertToImage(n,n.width,n.height);r.style.cssText="\n    width:14.27vw;\n    height:14.27vw;\n    display:block;\n  ",console.log(r),document.querySelector(".QRcode").removeChild(n),document.querySelector(".QRcode").append(r),document.querySelector(".QRcode").removeChild(document.querySelector(".QRcode img"))}(),n={series:[{name:"中国",data:i.option,type:"map",mapType:"china",roam:!1,top:"3%",zoom:1.1,selectedMode:"single",label:{normal:{show:!1,textStyle:{color:"#000",fontSize:8,fontWeight:"bold"}},emphasis:{show:!1,textStyle:{color:"#000"}}},itemStyle:{normal:{borderWidth:.5,borderColor:"#c8c2c6",color:function(e){var t=e.data.value;return c[t>5e3?0:t>1e3?1:t>100?2:t>10?3:t>0?4:5]}},emphasis:{borderWidth:.5,borderColor:"#c8c2c6",color:function(e){var t=e.data.value;return c[t>5e3?0:t>1e3?1:t>100?2:t>10?3:t>0?4:5]}}},markPoint:{symbol:"circle",symbolSize:8,label:{normal:{show:!1},emphasis:{show:!1}},itemStyle:{normal:{color:35!==i.province_id&&"海外"!==i.province?"rgba(172,50,28,1)":"rgba(0, 0, 0, 0)"}},data:(r=[],r.push({coord:[i.longitude,i.latitude]}),r)}}]},a.setOption(n),a.off("click"),sessionStorage.setItem("need-refresh",!0),window.onload=function(){setTimeout((function(){!function(){var e=document.body,t=e.offsetWidth,o=e.offsetHeight,n=document.createElement("canvas");n.width=String(2*+t),n.height=String(2*+o),console.log(n.width,n.height),n.getContext("2d").scale(2,2),console.log(t),console.log(o);var r=(a={scale:2,canvas:n,allowTaint:!0,width:t,height:o,dpi:4,useCORS:!0},i="scale",c=1,i in a?Object.defineProperty(a,i,{value:c,enumerable:!0,configurable:!0,writable:!0}):a[i]=c,a);var a,i,c;html2canvas(e,r).then((function(e){var t=e.getContext("2d");t.mozImageSmoothingEnabled=!1,t.webkitImageSmoothingEnabled=!1,t.msImageSmoothingEnabled=!1,t.imageSmoothingEnabled=!1;var o=e.toDataURL("jpg"),n=document.createElement("img");n.src=o,n.style.cssText="\n    width:100vw;\n    height:100vh;\n    position:absolute;\n    top:0;\n    left:0;\n    z-index:999;\n    opacity:0;\n    ",document.body.append(n),console.log(n)}))}()}),500)}}]);
-//# sourceMappingURL=share.js.map
+
+let myChart = echarts.init(document.getElementById('china-map'));
+
+let grobal = JSON.parse(window.sessionStorage.getItem('grobal'));
+
+let colorList = ['#29454c', '#4c6b73', '#819d9e','b2ced8v', '#vced3e0', '#eeeeee'];
+
+let provinces = ['shanghai', 'hebei', 'shanxi', 'neimenggu', 'liaoning', 'jilin', 'heilongjiang', 'jiangsu', 'zhejiang', 'anhui', 'fujian', 'jiangxi', 'shandong', 'henan', 'hubei', 'hunan', 'guangdong', 'guangxi', 'hainan', 'sichuan', 'guizhou', 'yunnan', 'xizang', 'shanxi1', 'gansu', 'qinghai', 'ningxia', 'xinjiang', 'beijing', 'tianjin', 'chongqing', 'xianggang', 'aomen'];
+
+let provincesText = ['上海', '河北', '山西', '内蒙古', '辽宁', '吉林', '黑龙江', '江苏', '浙江', '安徽', '福建', '江西', '山东', '河南', '湖北', '湖南', '广东', '广西', '海南', '四川', '贵州', '云南', '西藏', '陕西', '甘肃', '青海', '宁夏', '新疆', '北京', '天津', '重庆', '香港', '澳门'];
+// 初始化echarts
+initQRcode(); // 初始化二维码
+
+initEcharts();
+
+sessionStorage.setItem("need-refresh", true);
+window.onload = function () {
+  setTimeout(() => {
+    convert2canvas();
+  }, 500); // 无法取消echarts散点的动画,通过延迟截屏,来截取红点
+}
+
+function initQRcode() { // 初始化
+  console.log(grobal);
+  var img = new Image();
+  // 关键点，设置crossOriginal属性
+  img.setAttribute('crossOrigin', 'anonymous');
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext('2d');
+  img.src = grobal.avatar;
+  img.onload = function () {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+  }
+  // let portaitImg = canvas.convertToImage(canvas, canvas.width, canvas.height);
+  // portaitImg.style.cssText = 
+  // 'width:100%;height:100%;'
+  // ;
+  // document.querySelector('.portait').removeChild(canvas);
+  // document.querySelector('.portait').append(portaitImg);
+  // document.querySelector('.portait').append(document.querySelector('.portait img'));
+  // //document.querySelector('.portait > img').setAttribute('src', grobal.avatar);
+  document.querySelector('.message').innerHTML = `<div>我在${grobal.province}</div><div>我为湖北加油! 我为中国加油!</div>`;
+  console.log(window.location);
+  let QRcode =  new QRCode(document.querySelector(".QRcode"),{
+                                                              text: window.location.href.split('?')[0].replace('share.html', 'index.html'),// 项目首目录记得改掉
+                                                              colorDark: '#000000',
+                                                              colorLight: '#ffffff',
+                                                              correctLevel: QRCode.CorrectLevel.H
+                                                            });
+  let canvaQRcode = document.querySelector('.QRcode > canvas');
+  let imgQRcode = Canvas2Image.convertToImage(canvaQRcode, canvaQRcode.width, canvaQRcode.height);
+  imgQRcode.style.cssText = `
+    width:14.27vw;
+    height:14.27vw;
+    display:block;
+  `
+  console.log(imgQRcode);
+  document.querySelector('.QRcode').removeChild(canvaQRcode);
+  document.querySelector('.QRcode').append(imgQRcode);
+  document.querySelector('.QRcode').removeChild(document.querySelector('.QRcode img'));
+
+}
+function initEcharts() {
+  let option = {
+    series: [
+      {
+        name: '中国',
+        data: grobal.option,
+        type: 'map',
+        mapType: 'china',
+        roam: false,//是否开启鼠标缩放和平移漫游
+        top: "3%",//组件距离容器的距离
+        zoom:1.1,
+        selectedMode : 'single',
+        label: {
+          normal: {
+            show: false,//显示省份标签
+            textStyle:{
+              color: '#000',
+              fontSize: 8,
+              fontWeight: 'bold',
+            }//省份标签字体样式
+          },
+          emphasis: {//对应的鼠标悬浮效果
+            show: false,
+            textStyle: {
+              color: '#000'
+            }
+          }
+        },
+        itemStyle: {
+          normal: {
+            borderWidth: .5,//区域边框宽度
+            borderColor: '#c8c2c6',//区域边框颜色
+            color: 
+              function (params) { // 设置颜色
+                let itemValue = params.data.value;
+                let index = 0;
+                if (itemValue > 5000) index = 0;
+                else if (itemValue > 1000) index = 1;
+                else if (itemValue > 100) index =2;
+                else if (itemValue > 10) index = 3;
+                else if (itemValue > 0) index = 4;
+                else index = 5;
+                return colorList[index];
+              }
+          },
+          emphasis: {
+            borderWidth: .5,
+            borderColor: '#c8c2c6',
+            color:
+              function (params) { // 设置颜色
+                let itemValue = params.data.value;
+                let index = 0;
+                if (itemValue > 5000) index = 0;
+                else if (itemValue > 1000) index = 1;
+                else if (itemValue > 100) index =2;
+                else if (itemValue > 10) index = 3;
+                else if (itemValue > 0) index = 4;
+                else index = 5;
+                return colorList[index];
+              }
+          }
+        },
+        markPoint: { //图表标注。
+          symbol: 'circle',
+          symbolSize: 8,//图形大小
+          label: {
+              normal: {
+                show: false,
+              },
+              emphasis: {
+                show: false,
+              }
+          },
+          itemStyle: {
+            normal: {
+              color: 
+              (function () {
+                if(grobal.province_id !== 35 && grobal.province !== "海外") {
+                  return 'rgba(172,50,28,1)'
+                } else {
+                  return 'rgba(0, 0, 0, 0)'
+                }
+              })(),
+            }
+          },
+          data: (function () {
+            let coordArr = [];
+            coordArr.push({coord: [grobal.longitude, grobal.latitude]});
+            return coordArr;
+          })()
+        }
+      }
+    ]
+  };
+  myChart.setOption(option);
+  myChart.off("click");
+}
+function convert2canvas() {
+  // 获取需要转化的dom对象 直接使用$('.wrap')选取的为jquery对象 无法继续操作
+  var cntElem = document.body;
+  var shareContent = cntElem; //需要截图的包裹的（原生的）DOM 对象
+  var width = shareContent.offsetWidth; //获取dom 宽度
+  var height = shareContent.offsetHeight; //获取dom 高度
+  var canvas = document.createElement("canvas"); //创建一个canvas节点
+  var scale = 2; //定义任意放大倍数 支持小数
+  canvas.width = String(+width * scale); //定义canvas 宽度 * 缩放
+  canvas.height = String(+height * scale); //定义canvas高度 *缩放
+  console.log(canvas.width, canvas.height);
+  //放大后再缩小提高清晰度
+  canvas.getContext("2d").scale(scale, scale);
+  //document.body.append(canvas);
+  console.log(width)
+  console.log(height)
+  // 设置html2canvas方法的配置
+  var opts = {
+    scale: scale, // 添加的scale 参数
+    canvas: canvas, //自定义 canvas
+    allowTaint: true, //允许画布上有跨域图片 不建议使用 后面详细补充
+    // logging: true, //日志开关，便于查看html2canvas的内部执行流程
+    width: width, //dom 原始宽度
+    height: height,
+    dpi: 4, // 设置像素点,增加清晰度
+    useCORS: true, // 【重要】开启跨域配置
+    scale:1 // 设置倍率
+  };
+  // 开始转化为canvs对象
+  html2canvas(shareContent, opts).then(function(canvas) {
+
+    var context = canvas.getContext('2d');
+    // 【重要】关闭抗锯齿
+    context.mozImageSmoothingEnabled = false;
+    context.webkitImageSmoothingEnabled = false;
+    context.msImageSmoothingEnabled = false;
+    context.imageSmoothingEnabled = false;
+
+    // 【重要】默认转化的格式为png,也可设置为其他格式
+    let imgUrl = canvas.toDataURL('jpg'); // 将图片在放大的canvas画出后,要导入url,不然图片只有一部分
+    var img = document.createElement('img');
+    img.src=imgUrl;
+    img.style.cssText = `
+    width:100vw;
+    height:100vh;
+    position:absolute;
+    top:0;
+    left:0;
+    z-index:999;
+    opacity:0;
+    `
+    document.body.append(img);
+    console.log(img);
+  })
+}
